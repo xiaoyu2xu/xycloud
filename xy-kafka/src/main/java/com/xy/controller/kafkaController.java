7435bf386a783db89790e1e810e02c194deba212
zhangxy <q214527745@163.com>
2019-12-09 17:22:35 +0800

    命名修改

--- xy-kafka/src/main/java/com/xy/controller/kafkaController.java
+++ xy-kafka/src/main/java/com/xy/controller/kafkaController.java
@@ -1,11 +1,13 @@
 package com.xy.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class kafkaContrller {
+@Api(value = "kafka管理服务", tags = {"kafka管理"})
+public class kafkaController {
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
